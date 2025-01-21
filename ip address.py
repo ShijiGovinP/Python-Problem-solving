1607cb2eb04bf02e500888ed1ad46a792b9ab50d
ShijiGovinP <shijigovin2004@gmail.com>
2025-01-21 21:52:50 +0530

    Update ip address.py
    The find function checks if a string is a valid IPv4 address,It splits the input by dots (.) into four parts. Each part must be numeric and between 0 and 255. If these conditions are met, it returns true, Otherwise, it returns False.

--- ip address.py
+++ ip address.py
@@ -1,4 +1,4 @@
-def is_valid_ipv4(ip):
+def find(ip):
     segments = ip.split('.')
     if len(segments) != 4:
         return False
@@ -6,8 +6,8 @@ def is_valid_ipv4(ip):
         if not segment.isdigit() or not (0 <= int(segment) <= 255):
             return False
     return True
-ip_address = "192.163.72.102"
-if is_valid_ipv4(ip_address):
-    print(f"{ip_address} is a valid IPv4 address.")
+ipaddress = "192.163.72.102"
+if find(ipaddress):
+    print(f"{ipaddress} is a valid IPv4 address.")
 else:
-    print(f"{ip_address} is not a valid IPv4 address.")
+    print(f"{ipaddress} is not a valid IPv4 address.")
