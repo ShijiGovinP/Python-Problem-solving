77a6f07f1aca195c43d4b3a27985668006e75204
ShijiGovinP <shijigovin2004@gmail.com>
2025-01-22 20:04:51 +0530

    Update sum of natural numbers.py

--- sum of natural numbers.py
+++ sum of natural numbers.py
@@ -1,5 +1,6 @@
 n = int(input("Enter a natural number: "))
 if n > 0:
-    print(f"The sum of the first {n} natural numbers is: {n * (n + 1) // 2}")
+    numbers = n * (n + 1) // 2
+    print("The sum of the first", n, "natural numbers is:",numbers)
 else:
-    print("Please enter a positive number.")
+    print("Please enter a positive number")
