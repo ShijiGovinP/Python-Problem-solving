042d61c9874606b20363e529f605a0d8ab0f3041
ShijiGovinP <shijigovin2004@gmail.com>
2025-01-23 21:55:58 +0530

    middle character.py

--- middle character.py
+++ middle character.py
@@ -1,13 +1,11 @@
-def find_middle_character(s):
+def find_(s):
     length = len(s)
     middle = length // 2 
-    
     if length % 2 == 0:  
         return s[middle - 1:middle + 1]
     else:  
         return s[middle]
 string1 = "hello"
 string2 = "python"
-
-print(f"Middle character(s) of '{string1}': {find_middle_character(string1)}")
-print(f"Middle character(s) of '{string2}': {find_middle_character(string2)}")
+print("Middle character(s) of " + string1 + "':", find_middle_character(string1))
+print("Middle character(s) of " + string2 + "':", find_middle_character(string2))
